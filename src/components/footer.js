bbc706fcdf03887725d8a0e4b91d7de5cb1d4095
Jaron Schulz <jaronchulz@gmail.com>
2019-09-03 17:14:24 +0200

    reset to zero - starting fresh

--- src/components/footer.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const Footer = () => {
-  return <div>HEY from Footer</div>
-}
-
-export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,3 @@
 import React from "react"
-// import { Link } from "gatsby"
-// import Image from "../components/image"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hello From Home</h1>
-  </Layout>
-)
-
-export default IndexPage
+export default () => <div>Hello world!</div>
