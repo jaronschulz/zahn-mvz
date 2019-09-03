17b5b8ca5c0208b81f4ad9e3c2a790754f3e96c1
Jaron Schulz <jaronchulz@gmail.com>
2019-09-03 17:31:30 +0200

    basic plugins installed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,1 +1,19 @@
-module.exports = {}
+module.exports = {
+  siteMetadata: {
+    title: `Zahn MVZ`,
+    siteUrl: `https://www.zahn-med-mvz.de.org`,
+    description: `Zahn- und kieferorthopädisches Versorgungszentrum für Menschen mit Behinderung`,
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+  ],
+}
